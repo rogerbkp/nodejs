03272388907676cd14cdd0f7294097a9ade16c3f
rogerbkp <145692400+rogerbkp@users.noreply.github.com>
2025-01-23 23:24:36 +0000

    Aulas 1 e 2

---
+++ aula-1/index.js
@@ -0,0 +1,21 @@
+
+console.log("Hello, World!")
+
+// Variável global
+{
+    var nome = "tux"
+}
+console.log(nome)
+
+// Variável mutável e imutável 
+
+// const nome2 = "multics"
+let nome2 = "multics"
+
+console.log(nome2)
+
+nome2 = "unix"
+console.log(nome2)
+
+const processCurrentWorkDirectory = process.cwd()
+console.log(processCurrentWorkDirectory)
